ac6189ce1e217c2dcc0e630c075cd8a8c56d0846
amruta-khamkar <amrutakhamkar15@gmail.com>
2021-12-15 02:17:44 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { Routes,Route } from 'react-router-dom';
+import Home from './Components/Home';
 import './App.css';
+import SignUp from './Components/SignUp';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
+import Menu from './Components/Menu';
+import Cart from './Components/Cart';
+import Checkout from './Components/Checkout';
+import Success from './Components/Success';
+import MyOrders from './Components/MyOrders';
+import Profile from './Components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Routes>
+       <Route path="/" element={<Home/>}></Route>
+       <Route path="/register" element={<SignUp/>}></Route>
+       <Route path="/login" element={<Login/>}></Route>
+       <Route path="/dashboard" element={<Dashboard/>}></Route>
+       <Route path="/menu" element={<Menu/>}></Route>
+       <Route path="/cart" element={<Cart/>}></Route>
+       <Route path="/checkout" element={<Checkout/>}></Route>
+       <Route path="/success" element={<Success/>}></Route>
+       <Route path="/myorders" element={<MyOrders/>}></Route>
+       <Route path="/profile" element={<Profile/>}></Route>
+     </Routes>
+
+    </>
   );
 }
 
